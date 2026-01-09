59bfc1cab757187cb444a597e796e8288455dbb1
suhani-kharode <suhanikharode3@gmail.com>
2026-01-09 22:02:16 +0530

    Develop amazom.js and cart.js file to add the functionality

---
+++ script/anazon.js
@@ -0,0 +1,35 @@
+//first we save the data
+//generate the html
+//make it functional
+///make it interactive
+//
+
+let products = [
+  name : "Black and Gray Athletic Cotton Socks",
+  image : "images/products/athletic-cotton-socks-6-pairs.jpg",
+  price : 10.90,
+  rating : {
+    stars : 4.5,
+    count : 87
+  }
+],
+
+let products = [
+  name : "Intermediate Size Basketball",
+  image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  price : 10.90,
+  rating : {
+    stars : 4.5,
+    count : 87
+  }
+],
+
+let products = [
+  name : "Black and Gray Athletic Cotton Socks",
+  image : "images/icons/checkmark.png",
+  price : 10.90,
+  rating : {
+    stars : 4.5,
+    count : 56
+  }
+],
